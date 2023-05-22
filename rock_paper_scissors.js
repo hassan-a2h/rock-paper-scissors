90fc8f51a54b71e35ae9867328a6ffbff0828f92
Ahmad Hassan <a2h.ahmadhassan@gmail.com>
2023-05-22 13:42:39 +0500

    Let user play rps and check score, refactor js file
    * Display result using two divs inside a flexbox
    * Stylize the divs containing the results
    * Refactor the rock_paper_scissors.js file, creating new functions and reducing redundancy

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -2,61 +2,67 @@ const rock = document.querySelector('#rock');
 const paper = document.querySelector('#paper');
 const scissors = document.querySelector('#scissors');
 
-let userMove = '',
-  computerMove = getComputerMove(),
-  winner = '';
-
 rock.addEventListener('click', () => {
-  const computerMove = getComputerMove(),
-  userMove = 'rock';
-  const winner = oneRound(userMove, computerMove);
-  displayWinner(winner);
+  game('rock');
 });
 
 paper.addEventListener('click', () => {
-  const computerMove = getComputerMove(),
-  userMove = 'paper';
-  const winner = oneRound(userMove, computerMove);
-  displayWinner(winner);
+  game('paper');
 });
 
 scissors.addEventListener('click', () => {
-  const computerMove = getComputerMove(),
-  userMove = 'scissors';
-  const winner = oneRound(userMove, computerMove);
-  displayWinner(winner);
+  game('scissors');
 });
+
+let userWins = 0,
+  computerWins = 0;
+
+function game(move) {
+  const computerMove = getComputerMove(),
+  userMove = move;
+  const winner = oneRound(userMove, computerMove);
+  displayScore();
+  displayWinner(winner);
+}
+
+function getComputerMove() {
+  const moves = ['rock', 'paper', 'scissors'];
+  return moves[Math.ceil(Math.random() * 3) - 1];
+}
+
+function oneRound(userMove, computerMove) {
+  if (userMove === computerMove) {
+    return 0;
+  }
+  else if ((userMove === 'rock' && computerMove === 'scissors') ||
+          (userMove === 'paper' && computerMove === 'rock') ||
+          (userMove === 'scissors' && computerMove === 'paper')) {
+    return 1;
+  }
+  else {
+    return 2;
+  }
+}
+
+function displayScore() {
+  const winCount = document.querySelector('.wins');
+  winCount.setAttribute('style', 'display: flex;');
+  winCount.innerHTML = `<p>User Wins: ${userWins} <br> Computer Wins: ${computerWins}</p>`;
+}
 
 function displayWinner(winner) {
   const announceWinner = document.querySelector('.round-winner');
+  announceWinner.style.cssText = 'display: flex;';
+
   if (winner === 1) {
     announceWinner.textContent = 'You Win';
+    userWins++;
   }
   else if (winner === 2) {
     announceWinner.textContent = 'Computer Wins';
+    computerWins++;
   }
   else {
     announceWinner.textContent = 'Draw Match';
   }
-}
-
-// Playing One Round
-function oneRound(userMove, computerMove) {
-  if (userMove === computerMove) {
-   return 0;
-  }
-  else if ((userMove === 'rock' && computerMove === 'scissors') ||
-           (userMove === 'paper' && computerMove === 'rock') ||
-           (userMove === 'scissors' && computerMove === 'paper')) {
-     return 1;
-   }
-   else {
-     return 2;
-   }
- }
-
- // Computer's Move
-function getComputerMove() {
-  const moves = ['rock', 'paper', 'scissors'];
-  return moves[Math.ceil(Math.random() * 3) - 1];
 }
